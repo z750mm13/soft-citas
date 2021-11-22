b1e9538f6daff1e5a9f424ad95ec65f511b9625a
z750mm13 <maurir_@hotmail.com>
2021-11-22 16:08:15 -0600

    Correccion de pagina principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container mt-5 mb-5">
     <div class="row">
         <div class="col-8">
             @include('dashboard.calendar')
