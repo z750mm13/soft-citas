39cfe0a06fbfd88cbaa6bee90f8b9797b18e21ad
z750mm13 <maurir_@hotmail.com>
2021-11-22 15:13:38 -0600

    Correccion del registro

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,8 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'lastname' => $data['lastname'],
+            'postlastname' => $data['postlastname'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
