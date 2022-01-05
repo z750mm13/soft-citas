4a2d5ff7b8938f2a8b0a8819ced3139a388dc873
z750mm13 <maurir_@hotmail.com>
2022-01-04 20:01:21 -0600

    Ajuste de lineas en la receta

--- app/Exports/PrescriptionExport.php
+++ app/Exports/PrescriptionExport.php
@@ -41,6 +41,27 @@ class PrescriptionExport implements FromView, WithEvents, WithDrawings {
                 $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setSize(16);
                 $event->sheet->getDelegate()->getStyle(str_replace($cellRange, '1', '2'))->getFont()->setSize(12);
                 $event->sheet->getDelegate()->getStyle(str_replace($cellRange, '1', '3'))->getFont()->setSize(12);
+                
+                $event->sheet->getStyle('A3:C3')->applyFromArray([
+                    'borders' => [
+                        'bottom' => [
+                            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                            'color' => ['argb' => '000000'],
+                        ],
+                    ],
+                ]);
+                $event->sheet->getStyle('D1:D3')->applyFromArray([
+                    'borders' => [
+                        'left' => [
+                            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                            'color' => ['argb' => '000000'],
+                        ],
+                        'bottom' => [
+                            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                            'color' => ['argb' => '000000'],
+                        ],
+                    ],
+                ]);
             },
         ];
     }
